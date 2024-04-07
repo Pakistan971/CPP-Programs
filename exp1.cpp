5de07397df96d01417adca31af2e689a583a7ca4
Pakistan971 <161756431+Pakistan971@users.noreply.github.com>
2024-04-07 13:02:11 +0500

    Add files via upload

--- exp1.cpp
+++ exp1.cpp
@@ -1,76 +1,19 @@
-// 1.	Define a class Customer that holds private fields for a customer's ID, first name, last name, address and credit limit.
-// Define functions that set the fields of customer. For example setCustomerID(). 
-// Define functions that show the fields of customer. For example getCustomerID().
-// Use constructor to set the default values to each of the field.
-// Overload at least six constructor of the customer class.
-// Define a function that takes input from user and set the all fields of customer data.
-// Define a function that displays the entire customer’s data. 
+//1.	Write a program that find the largest number in an array.
 #include<iostream>
 using namespace std;
-class customer{
-    private:
-        int CustomerID;
-        string CustomerFirstName;
-        string CustomerLastName;
-        string CustomerAddress;
-        float Limit;
-    public:
-    void setValues(int id, string cfn,string cln, string cadd,int lim){
-        CustomerID = id;
-        CustomerFirstName = cfn;
-        CustomerLastName = cln;
-        CustomerAddress = cadd;
-        Limit = lim;
+void largest_in_array(int arr[],int len){
+    int max=arr[0];
+    for(int i=0;i<len;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
     }
-    int idReturn(){
-        return CustomerID;
-    }
-    int LimitReturn(){
-        return Limit;
-    }
-    string FirstNameReturn(){
-        return CustomerFirstName;
-    }
-    string LastNameReturn(){
-        return CustomerLastName;
-    }
-    string AddressReturn(){
-        return CustomerAddress;
-    }
-};
-void InputCustomerData(customer &customerObj){
-    int id,lim;
-    string cfn,cln,cadd;
-    cout<<"Enter the Customer ID: ";
-    cin>>id;
-    cout<<"Enter the Customer First Name: ";
-    cin>>cfn;
-    cout<<"Enter the Customer Last Name: ";
-    cin>>cln;
-    cout<<"Enter the Customer Address: ";
-    cin>>cadd;
-    cout<<"Enter the Customer Limit: ";
-    cin>>lim;
-    customerObj.setValues(id,cfn,cln,cadd,lim);
-}
-void OutputCustomerData(customer &Customer){
-    cout<<"The Customer ID: "<<Customer.idReturn()<<endl;
-    cout<<"The Customer Fist Name: "<<Customer.FirstNameReturn()<<endl;
-    cout<<"The Customer Last Name: "<<Customer.LastNameReturn()<<endl;
-    cout<<"The Customer Address: "<<Customer.AddressReturn()<<endl;
-    cout<<"The Customer Limit: "<<Customer.LimitReturn()<<endl;
+    cout<<max<<" is the largest among all the Integers in The Array.";
 }
 int main()
 {
-    customer customer1,customer2;
-    InputCustomerData(customer1);
-    cout<<endl<<"<-------------------------The Output IS------------------------->"<<endl;
-    OutputCustomerData(customer1);
-    cout<<"<-------------------------THE END------------------------->"<<endl;
-    InputCustomerData(customer2);
-    cout<<endl<<"<-------------------------The Output IS------------------------->"<<endl;
-    OutputCustomerData(customer2);
-    cout<<"<-------------------------THE END------------------------->"<<endl;
-    cout<<"The Customer ID: "<<customer1.idReturn()<<endl;
+    int arr[10]={1,2,3,47,5,6,7,8,9,10};
+    int length_of_array = sizeof(arr)/sizeof(arr[0]);
+    largest_in_array(arr,length_of_array);
     return 0;
 }
